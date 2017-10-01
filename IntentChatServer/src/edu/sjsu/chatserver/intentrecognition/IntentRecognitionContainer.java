5bfb412f9be0dc42c7dbdfed8bd8e85da8020be3
kjayashankar <kjayashankar@yahoo.com>
2017-10-01 18:16:39 +0530

    modified rabbitmq

--- IntentChatServer/src/edu/sjsu/chatserver/intentrecognition/IntentRecognitionContainer.java
+++ IntentChatServer/src/edu/sjsu/chatserver/intentrecognition/IntentRecognitionContainer.java
@@ -1,18 +1,30 @@
 package edu.sjsu.chatserver.intentrecognition;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+
 @Path("/intent")
 public final class IntentRecognitionContainer {
 
+	private static final int SUCCESS_CODE = 200;
+	private static final String SUCCESS_MSG = "success";
+	
 	@GET
 	@Path("/{chatString}")
 	public Response getMsg(@PathParam("chatString") String chatString) {
 		String output = "Classification for chat : " + chatString;
-		return Response.status(200).entity(output).build();
+		return Response.status(SUCCESS_CODE).entity(output).build();
+	}
+	
+	@POST
+	@Path("/updateCorpus")
+	public Response updateCorpus(String sentence, String classifier){
+		
+		return Response.status(SUCCESS_CODE).entity(SUCCESS_MSG).build();
 	}
 	
 }
---
+++ IntentChatServer/src/edu/sjsu/chatserver/process/DataCorpus.java
@@ -0,0 +1,69 @@
+package edu.sjsu.chatserver.process;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class DataCorpus {
+
+	private static final String ENTRY = "{\"class\":\"?\", \"sentence\":\"*\"}";
+	private static int classIndex = ENTRY.indexOf("?");
+	private static int msgIndex = ENTRY.indexOf("*");
+
+	public static void appendCorpus(String message, String classifier) {
+		String data = prepareCorpusEntry(message, classifier);
+		addToMessageQueue(data);
+	}
+
+	private static void addToMessageQueue(String data) {
+
+		Channel channel = null;
+		Connection connection = null;
+		String EXCHANGE_NAME = "EXCHANGE";
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+
+			channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+			String message = data;
+			channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+			System.out.println(" [x] Sent '" + message + "'");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				channel.close();
+				connection.close();
+			} catch (IOException | TimeoutException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		/*
+		 * FileWriter writer = null; try { File f = new
+		 * File(CORPUS_TEMP_LOCATION); writer = new
+		 * FileWriter(CORPUS_TEMP_LOCATION, true); if(!f.canWrite()) {
+		 * System.out.
+		 * println("might be accessed by other application, lets wait!");
+		 * Thread.sleep(100); } writer.write(data); writer.flush();
+		 * writer.close(); } catch (Exception e) { e.printStackTrace(); }
+		 * finally { if (writer != null) { try { writer.flush(); writer.close();
+		 * } catch (IOException e) { e.printStackTrace(); } } }
+		 */
+	}
+
+	private static String prepareCorpusEntry(String message, String classifier) {
+
+		StringBuffer buffer = new StringBuffer();
+
+		buffer.append(ENTRY.substring(0, classIndex)).append(classifier).append(ENTRY.substring(classIndex, msgIndex))
+				.append(message).append(ENTRY.substring(msgIndex));
+		return buffer.toString();
+	}
+}
